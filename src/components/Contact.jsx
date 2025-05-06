e4a6d57d530fd5691dde74f3f4fdb222f1d855c1
KITTIPAT-BIED <kittipat.kamlea1999@example.com>
2025-05-07 00:40:39 +0700

    i

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
     <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Contact</h1>
     <div className='container text-gray-500 mx-auto py-10 px-6 lg:px-32 w-full overflow-hidden'>
         <p className='text-xl text-gray-700 mt-2'>Name: Kittipat kamlue</p>
-        <p className='text-xl text-gray-700 mt-2'>Email: kittipat.kamlea2002@bumail.net</p>
+        <p className='text-xl text-gray-700 mt-2'>Email: kittipat.kamlea2002@gmail.com</p>
         <p className='text-xl text-gray-700 mt-2'>Phone: 0933916717</p>
         <p className='text-xl text-gray-700 mt-2'>Linkedin: https://www.linkedin.com/in/kittipat-kamlue-104760327/</p>
         <p className='text-xl text-gray-700 mt-2'>Github: KITTIPAT-BIRD</p>
